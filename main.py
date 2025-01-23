d7a6c8913c51e442693bf580103ae2d7ba616851
tuwi <0808374@hr.nl>
2025-01-23 14:30:03 +0100

    names dict

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "hello world"}
+names = {
+    "tim": {"age": 12, "gender": "Male"},
+    "tuwi": {"age": 234, "gender": "Female"},
+         }
 
-api.add_resource(HelloWorld, '/helloworld')
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+    def post(self):
+        return {"data": "posted"}
+
+api.add_resource(HelloWorld, '/helloworld/<string:name>')
 
 if __name__ == "__main__":
     app.run(debug=True)
