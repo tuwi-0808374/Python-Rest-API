4923f8dfb9ff133094df76ad3b14939fcabb58fc
tuwi <0808374@hr.nl>
2025-01-28 21:40:19 +0100

    creating a model,

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+import os
 
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<VideoModel {self.name}, {self.views} views, {self.likes} likes>"
+
+# Check if database exists before creating it
+if not os.path.exists("database.db"):
+    print("Database does not exist. Creating...")
+    with app.app_context():
+        db.create_all()  # Creates the tables
+else:
+    print("Database already exists. Skipping creation.")
+
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video")
@@ -11,14 +33,31 @@ video_put_args.add_argument("likes", type=int, help="Likes on the video")
 
 videos = {}
 
+def abort_if_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message = f"Video with id {video_id} doesn't exist")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message = f"Video with id {video_id} already exists")
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id)
+
         args = video_put_args.parse_args()
         videos[video_id] = args
+
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return "", 204
 
 api.add_resource(Video, '/video/<int:video_id>')
 
--- test.py
+++ test.py
@@ -2,8 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
-print(response.json())
-input()
-response = requests.get(BASE + "video/1")
+data =  [
+    {"name": "HOT STUFF SEE NOW", "views": 123, "likes": 590},
+    {"name": "Breaking news", "views": 5678, "likes": 33330},
+    {"name": "How to do something", "views": 512453453, "likes": 1200}
+]
+
+for i in range(0, len(data)):
+    response = requests.put(BASE + "video/"  + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "video/0")
+print(response)
+response = requests.get(BASE + "video/6")
 print(response.json())
